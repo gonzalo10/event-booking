8bc8994d648e9adaee7c09e9177283bbf7d4cc98
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-03-10 20:48:07 +0100

    tutorial 5 finished mongoDB install

--- App.js
+++ App.js
@@ -14,29 +14,29 @@ app.use(
     '/graphql',
     graphqlHttp({
       schema: buildSchema(`
-          type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-          }
-          input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-          }
-          type RootQuery {
-              events: [Event!]!
-          }
-          type RootMutation {
-              createEvent(eventInput: EventInput): Event
-          }
-          schema {
-              query: RootQuery
-              mutation: RootMutation
-          }
+        type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        }
+        input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
       `),
     rootValue: {
         events: () => {
