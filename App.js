dc182537fe0a91baad53492bbdf5560abb82aa0e
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-03-10 22:12:35 +0100

    user creator working

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const Event = require('./models/events');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,23 +18,34 @@ app.use(
     graphqlHttp({
       schema: buildSchema(`
         type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
         input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
         }
         type RootQuery {
             events: [Event!]!
         }
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema {
             query: RootQuery
@@ -67,11 +81,33 @@ app.use(
               console.log(err);
               throw err;
             });
-        }
-      },
-      graphiql: true
-    })
-  );
+        },
+        createUser: args => {
+            return User.findOne({ email: args.userInput.email })
+              .then(user => {
+                if (user) {
+                  throw new Error('User exists already.');
+                }
+                return bcrypt.hash(args.userInput.password, 12);
+              })
+              .then(hashedPassword => {
+                const user = new User({
+                  email: args.userInput.email,
+                  password: hashedPassword
+                });
+                return user.save();
+              })
+              .then(result => {
+                return { ...result._doc, password: null, _id: result.id };
+              })
+              .catch(err => {
+                throw err;
+              });
+          }
+        },
+        graphiql: true
+      })
+    );
 
 mongoose
     .connect(`mongodb+srv://${process.env.MONGO_USER}:${
