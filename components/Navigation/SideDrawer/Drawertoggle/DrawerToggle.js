684ed5ecd559978575bc6e2dbf8f97a8d32fc984
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-04-20 15:48:10 +0200

    fixes

--- components/Navigation/SideDrawer/Drawertoggle/DrawerToggle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './DrawerToggle.css'
-
-const drawerToggle = (props) => (
-    <div className={classes.DrawerToggle} onClick={props.clicked}>Open Menu</div>
-);
-
-export default drawerToggle;
